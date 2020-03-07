d89992981601d305430adde40ef78c926ec4e2e0
Sam Keddy <sam@samkeddy.com>
2020-03-07 22:46:25 +0000

    updates

--- js/_fileMenu.js
+++ js/_fileMenu.js
@@ -2,41 +2,41 @@ var mainMenuItems = document.getElementById("main-menu").children;
 
 //for each button in main menu (starting at 1 to avoid logo)
 for (var i = 1; i < mainMenuItems.length; i++) {
-	
+
 	//get the button that's in the list item
 	var menuItem = mainMenuItems[i];
 	var menuButton = menuItem.children[0];
-	
+
 	console.log(mainMenuItems)
-	
+
     //when you click a main menu items button
     on('click', menuButton, function (e, button) {
         console.log('parent ', button.parentElement)
     		select(button.parentElement);
     });
-    
+
     var subMenu = menuItem.children[1];
     var subMenuItems = subMenu.children;
-    
-    
-    
+
+
+
     //when you click an item within a menu button
     for (var j = 0; j < subMenuItems.length; j++) {
-    	
+
     	var subMenuItem = subMenuItems[j];
     	var subMenuButton = subMenuItem.children[0];
-    	
-		subMenuButton.addEventListener("click", function () {
-				
+
+		subMenuButton.addEventListener("click", function (e) {
+
 			switch(this.textContent) {
-			  
+
 			  //File Menu
 				case 'New':
 					showDialogue('new-pixel');
 					break;
 				case 'Open':
-				  
-				  
+
+
 					//if a document exists
 					if (documentCreated) {
 					  //check if the user wants to overwrite
@@ -47,12 +47,12 @@ for (var i = 1; i < mainMenuItems.length; i++) {
   				else
   					//open file selection dialog
   					document.getElementById("open-image-browse-holder").click();
-  					
+
 					break;
-					
+
 				case 'Save as...':
 					if (documentCreated) {
-						
+
 						//create name
 						var selectedPalette = getText('palette-button');
 						if (selectedPalette != 'Choose a palette...'){
@@ -62,38 +62,34 @@ for (var i = 1; i < mainMenuItems.length; i++) {
 							var fileName = 'pixel-'+canvasSize[0]+'x'+canvasSize[1]+'.png';
 							selectedPalette = 'none';
 						}
-						
+
 						//set download link
 						var linkHolder = document.getElementById("save-image-link-holder");
 		                linkHolder.href = canvas.toDataURL();
 	    				linkHolder.download = fileName;
-	    				
+
 	    				linkHolder.click();
-	    				
+
 	    				//track google event
                         ga('send', 'event', 'Pixel Editor Save', selectedPalette, canvasSize[0]+'/'+canvasSize[1]); /*global ga*/
 					}
-					
+
 					break;
-					
+
 				case 'Exit':
+
+					console.log('exit')
 					//if a document exists, make sure they want to delete it
-					if (documentCreated && confirm('Exiting will discard your current pixel. Are you sure you want to do that?')) {
-    				
-    				//skip onbeforeunload prompt
-    				window.onbeforeunload = null;
-    				
-    				//go back to main site
-    				window.location.href = "https://lospec.com/pixel-editor";
+					if (documentCreated) {
+
+						//ask user if they want to leave
+						if (confirm('Exiting will discard your current pixel. Are you sure you want to do that?'))
+		    				//skip onbeforeunload prompt
+		    				window.onbeforeunload = null;
+						else
+							e.preventDefault();
 					}
-  				else {
-  				  
-  				  //skip onbeforeunload prompt
-    				window.onbeforeunload = null;
-    				
-  					//go back to main site
-    				window.location.href = "https://lospec.com/pixel-editor";
-  				}
+
 				  break;
 				//Edit Menu
 				case 'Undo':
@@ -102,17 +98,17 @@ for (var i = 1; i < mainMenuItems.length; i++) {
 				case 'Redo':
 					redo();
 					break;
-					
+
 				//Palette Menu
 				case 'Add color':
         	addColor('#eeeeee');
 					break;
 				//Help Menu
 				case 'Settings':
-				  
+
 				  //fill form with current settings values
 				  setValue('setting-numberOfHistoryStates', settings.numberOfHistoryStates);
-				  
+
 					showDialogue('settings');
 					break;
 				//Help Menu
@@ -126,7 +122,7 @@ for (var i = 1; i < mainMenuItems.length; i++) {
 					showDialogue('changelog');
 					break;
 			}
-			
+
 			closeMenu();
 		});
 	}
@@ -137,4 +133,4 @@ function closeMenu () {
 	for (var i = 0; i < mainMenuItems.length; i++) {
 	    deselect(mainMenuItems[i]);
 	}
-}	
+}
