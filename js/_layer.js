51d86cee77cd491cc97094fc4ef4c799631e39de
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2020-06-17 23:09:11 +0200

    Continued basic layer layout
    Added "Add layer" button, must find a nice way to add svgs

--- js/_layer.js
+++ js/_layer.js
@@ -1,10 +1,42 @@
 /** TODO LIST FOR LAYERS
+    
+    GENERAL REQUIREMENTS:
+    
+    - Must add a new layer (a new <li> element with class layer-menu-entry) when clicking on the "add" button
+    - Must delete the selected layer when right clicking on a layer and selecting that option
+        * We should think about selecting more than one layer at once.
+        * Merge with bottom layer option
+        * Flatten visible option
+        * Flatten everything option
+    - Must move a layer when dragging it in the layer list (https://codepen.io/retrofuturistic/pen/tlbHE)
+    - When the user clicks on the eye icon, the layer becomes transparent
+    - When the user clicks on the lock icon, the layer is locked
+    - Lock and visibility options are only shown on mouse hover
+    - When a layer is locked or not visible, the corresponding icons are always shown
+    - When a layer is selected, its background colour becomes lighter
+    - When saving an artwork, the layers must be flattened to a temporary layer, which is then exported and deleted
+    - Saving the state of an artwork to a .lospec file so that people can work on it later keeping 
+      the layers they created? That'd be cool, even for the app users, that could just double click on a lospec
+      file for it to be opened right in the pixel editor
+    
+    ADDITIONAL LOGIC TO MAKE IT WORK WITH THE REST OF THE CODE:
+
     - move the tmp layer so that it's always right below the active layer
     - when the move tool is selected (to move a selection), the tmp layer must be put right above the 
         active layer to show a preview
     - mouse events will always have at least a canvas target, so evey time there's an event, we'll have to check
         the actual element type instead of the current layer and then apply the tool on the currentLayer, not on
         the first one in order of z-index   
+    - when zooming in and out, all the layers should be scaled accordingly
+    - when changing the stroke colour, it should change for all the layers: it should already be happening,
+      just make sure it is
+
+    OPTIONAL:
+
+    1 - Fix issues 
+    2 - Add a Replace feature so that people can replace a colour without editing the one in the palette
+        (right click->replace colour in layers? in that case we'd have to implement multiple layers selection)
+
 */
 
 
