1e6f719f50d343c79d1b656c0e7484bbebd3753d
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2020-07-22 23:29:27 +0200

    Started resize canvas interface

---
+++ js/_resizeCanvas.js
@@ -0,0 +1,29 @@
+let resizeCanvasContainer = document.getElementById("resize-canvas");
+
+function openResizeCanvasWindow() {
+    showDialogue('resize-canvas');
+}
+
+// Use a dialogue
+    // Let the user specify data
+        // Borders
+        // Pivot
+    // Apply the new sizing
+    // Move the images on the layers to fit what the user actually decided
+
+// Function to handle dropdown menu for pivots
+    // Open when clicking on the button
+    // Save the chosen pivot
+    // Close the menu
+
+// Function to retrieve data from the input field
+    // Store the data in the form into variables
+    // Call the function to resize the canvases
+
+// Function to actually resize the canvases
+    // Save all imageDatas
+    // Resize the canvases
+    // Clear the canvases
+
+// Function to place the imageDatas as specified in the pivot section
+    // Put the imageDatas in the right position
