e213acea7673b4bee0e8f5cfcac6fe0bcbb018e7
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2020-09-18 15:22:10 +0200

    Added resize sprite window opening

--- js/_resizeSprite.js
+++ js/_resizeSprite.js
@@ -1,6 +1,34 @@
+let resizeSpriteInitialized = false;
+
 // Function to show dialogue
     // New size
     // Percentage change
     // Keep ratio checkbox
     // Choose resize algorithm
     // Confirm
+
+    function openResizeSpriteWindow() {
+        if (!resizeSpriteInitialized) {
+            resizeSpriteInitialized = true;
+            initResizeSpriteInputs();
+        }
+        showDialogue('resize-sprite');
+    }
+    
+    function initResizeSpriteInputs() {
+    /*
+        document.getElementById("rc-width").value = layers[0].canvasSize[0];
+        document.getElementById("rc-height").value = layers[0].canvasSize[1];
+    
+        document.getElementById("rc-border-left").addEventListener("change", changedBorder);
+        document.getElementById("rc-border-right").addEventListener("change", changedBorder);
+        document.getElementById("rc-border-top").addEventListener("change", changedBorder);
+        document.getElementById("rc-border-bottom").addEventListener("change", changedBorder);
+    
+        document.getElementById("rc-width").addEventListener("change", changedSize);
+        document.getElementById("rc-height").addEventListener("change", changedSize);
+    
+        document.getElementById("resize-canvas-confirm").addEventListener("click", resizeCanvas);
+        console.log("Pivot selezionato: " + currentPivotObject);
+        */
+    }
