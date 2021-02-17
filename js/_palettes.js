1302fc9bd644687cde268ba20881e9ebe3dca40a
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2021-02-17 18:59:49 +0100

    Started binding splash page events (new custom pixel section)

--- js/_palettes.js
+++ js/_palettes.js
@@ -1,40 +1,62 @@
+const { on } = require("gulp");
+
 //populate palettes list in new pixel menu
 Object.keys(palettes).forEach(function(paletteName,index) {
 
     var palettesMenu = document.getElementById('palette-menu');
+    var splashPalettes = document.getElementById('palette-menu-splash');
 
     //create button
     var button = document.createElement('button');
     button.appendChild(document.createTextNode(paletteName));
 
-    //insert new element
-    palettesMenu.appendChild(button);
-
     //if the palette was specified by the user, change the dropdown to it
     if (palettes[paletteName].specified == true) {
         setText('palette-button', paletteName);
+        setText('palette-button-splash', paletteName)
         //Show empty palette option
         document.getElementById('no-palette-button').style.display = 'block';
+        document.getElementById('no-palette-button-splash').style.display = 'block';
     }
 
-    on('click', button, function() {
+    var buttonEvent = function() {
 
         //hide the dropdown menu
         deselect('palette-menu');
         deselect('palette-button');
+        deselect('palette-menu-splash');
+        deselect('palette-button-splash');
 
         //show empty palette option
         document.getElementById('no-palette-button').style.display = 'block';
+        document.getElementById('no-palette-button-splash').style.display = 'block';
 
         //set the text of the dropdown to the newly selected preset
         setText('palette-button', paletteName);
-    });
+        setText('palette-button-splash', paletteName);
+    };
+
+    on('click', button, buttonEvent);
+
+    //insert new element
+    palettesMenu.appendChild(button);
+
+    // Making a copy for the splash page too
+    var copyButton = button.cloneNode(true);
+    // Attaching the same event
+    on('click', copyButton, buttonEvent);
+    // Appending it to the splash palette menu
+    splashPalettes.appendChild(copyButton);
 });
 
 //select no palette
 on('click', 'no-palette-button', function () {
     document.getElementById('no-palette-button').style.display = 'none';
     setText('palette-button', 'Choose a palette...');
+
+    // Same for splash page
+    document.getElementById('no-palette-button-splash').style.display = 'none';
+    setText('palette-button-splash', 'Choose a palette...');
 });
 
 //select load palette
@@ -43,13 +65,20 @@ on('click', 'load-palette-button', function () {
 });
 
 
-
 on('click', 'palette-button', function (e){
     toggle('palette-button');
     toggle('palette-menu');
 
     deselect('preset-button');
     deselect('preset-menu');
+
+    // Splash version
+    toggle('palette-button-splash');
+    toggle('palette-menu-splash');
+
+    deselect('preset-button-splash');
+    deselect('preset-menu-splash');
+
     e.stopPropagation();
 });
 
@@ -59,4 +88,10 @@ on('click', 'new-pixel', function (){
     deselect('preset-menu');
     deselect('palette-button');
     deselect('palette-menu');
+
+    // Splash version
+    deselect('palette-button-splash');
+    deselect('palette-menu-splash');
 });
+
+// ISSUE: use the same functions for the splash menu
