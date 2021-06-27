5eee1b941e455116befa94cc6dd88c76a326abb9
Leamsi Escribano <leamsi.esc@gmail.com>
2021-06-27 12:45:32 -0400

    IIFE and CSS Fixes

--- js/_presets.js
+++ js/_presets.js
@@ -1,63 +1,56 @@
-//presets
-var presets = {
-    'Gameboy Color': {
-        width: 240,
-        height: 203,
-        palette: 'Gameboy Color'
-    },
-    'PICO-8': {
-        width: 128,
-        height: 128,
-        palette: 'PICO-8'
-    },
-    'Commodore 64': {
-        width: 40,
-        height: 80,
-        palette: 'Commodore 64'
-    }
-};
+(() => {
+    const presets = {
+        'Gameboy Color': {
+            width: 240,
+            height: 203,
+            palette: 'Gameboy Color'
+        },
+        'PICO-8': {
+            width: 128,
+            height: 128,
+            palette: 'PICO-8'
+        },
+        'Commodore 64': {
+            width: 40,
+            height: 80,
+            palette: 'Commodore 64'
+        }
+    };
+    const presetsMenu = document.getElementById('preset-menu');
+    Object.keys(presets).forEach((presetName,) => {
 
-//populate preset list in new pixel menu
-Object.keys(presets).forEach(function(presetName,index) {
+        const button = document.createElement('button');
+        button.appendChild(document.createTextNode(presetName));
 
-    var presetsMenu = document.getElementById('preset-menu');
+        presetsMenu.appendChild(button);
 
-    //create button
-    var button = document.createElement('button');
-    button.appendChild(document.createTextNode(presetName));
+        button.addEventListener('click', () => {
+            //change dimentions on new pixel form
+            Utility().setValue('size-width', presets[presetName].width);
+            Utility().setValue('size-height', presets[presetName].height);
 
-    //insert new element
-    presetsMenu.appendChild(button);
+            //set the text of the dropdown to the newly selected preset
+            Utility().setText('palette-button', presets[presetName].palette);
 
-    //add click event listener
-    on('click', button, function() {
+            //hide the dropdown menu
+            Utility().deselect('preset-menu');
+            Utility().deselect('preset-button');
 
-        //change dimentions on new pixel form
-        setValue('size-width', presets[presetName].width);
-        setValue('size-height', presets[presetName].height);
+            //set the text of the dropdown to the newly selected preset
+            Utility().setText('preset-button', presetName);
 
-        //set the text of the dropdown to the newly selected preset
-        setText('palette-button', presets[presetName].palette);
-
-        //hide the dropdown menu
-        deselect('preset-menu');
-        deselect('preset-button');
-
-        //set the text of the dropdown to the newly selected preset
-        setText('preset-button', presetName);
+        });
     });
+    const presetButton = document.getElementById('preset-button');
+    presetButton.addEventListener('click', (e) => {
+        //open or close the preset menu
+        Utility().toggle('preset-button');
+        Utility().toggle('preset-menu');
 
-});
+        //close the palette menu
+        Utility().deselect('palette-button');
+        Utility().deselect('palette-menu');
 
-on('click', 'preset-button', function (e){
-    //open or close the preset menu
-    toggle('preset-button');
-    toggle('preset-menu');
-
-    //close the palette menu
-    deselect('palette-button');
-    deselect('palette-menu');
-
-    //stop the click from propogating to the parent element
-    e.stopPropagation();
-});
+        e.stopPropagation();
+    });
+})();
