bb8d5e467dc84495a29a8eef1872dd1813b7880f
Nicola <47360416+unsettledgames@users.noreply.github.com>
2021-06-27 17:46:22 +0200

    Revert "Performance Improvement and Code Structure Changes"

--- js/_presets.js
+++ js/_presets.js
@@ -1,56 +1,63 @@
-(() => {
-    const presets = {
-        'Gameboy Color': {
-            width: 240,
-            height: 203,
-            palette: 'Gameboy Color'
-        },
-        'PICO-8': {
-            width: 128,
-            height: 128,
-            palette: 'PICO-8'
-        },
-        'Commodore 64': {
-            width: 40,
-            height: 80,
-            palette: 'Commodore 64'
-        }
-    };
-    const presetsMenu = document.getElementById('preset-menu');
-    Object.keys(presets).forEach((presetName,) => {
-        
-        const button = document.createElement('button');
-        button.appendChild(document.createTextNode(presetName));
+//presets
+var presets = {
+    'Gameboy Color': {
+        width: 240,
+        height: 203,
+        palette: 'Gameboy Color'
+    },
+    'PICO-8': {
+        width: 128,
+        height: 128,
+        palette: 'PICO-8'
+    },
+    'Commodore 64': {
+        width: 40,
+        height: 80,
+        palette: 'Commodore 64'
+    }
+};
 
-        presetsMenu.appendChild(button);
+//populate preset list in new pixel menu
+Object.keys(presets).forEach(function(presetName,index) {
 
-        button.addEventListener('click', () => {
-            //change dimentions on new pixel form
-            Utility().setValue('size-width', presets[presetName].width);
-            Utility().setValue('size-height', presets[presetName].height);
+    var presetsMenu = document.getElementById('preset-menu');
 
-            //set the text of the dropdown to the newly selected preset
-            Utility().setText('palette-button', presets[presetName].palette);
+    //create button
+    var button = document.createElement('button');
+    button.appendChild(document.createTextNode(presetName));
 
-            //hide the dropdown menu
-            Utility().deselect('preset-menu');
-            Utility().deselect('preset-button');
+    //insert new element
+    presetsMenu.appendChild(button);
 
-            //set the text of the dropdown to the newly selected preset
-            Utility().setText('preset-button', presetName);
+    //add click event listener
+    on('click', button, function() {
 
-        });
+        //change dimentions on new pixel form
+        setValue('size-width', presets[presetName].width);
+        setValue('size-height', presets[presetName].height);
+
+        //set the text of the dropdown to the newly selected preset
+        setText('palette-button', presets[presetName].palette);
+
+        //hide the dropdown menu
+        deselect('preset-menu');
+        deselect('preset-button');
+
+        //set the text of the dropdown to the newly selected preset
+        setText('preset-button', presetName);
     });
-    const presetButton = document.getElementById('preset-button');
-    presetButton.addEventListener('click', (e) => {
-        //open or close the preset menu
-        Utility().toggle('preset-button');
-        Utility().toggle('preset-menu');
 
-        //close the palette menu
-        Utility().deselect('palette-button');
-        Utility().deselect('palette-menu');
+});
 
-        e.stopPropagation();
-    });
-})();
+on('click', 'preset-button', function (e){
+    //open or close the preset menu
+    toggle('preset-button');
+    toggle('preset-menu');
+
+    //close the palette menu
+    deselect('palette-button');
+    deselect('palette-menu');
+
+    //stop the click from propogating to the parent element
+    e.stopPropagation();
+});
