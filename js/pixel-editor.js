06d95368952bd2a914f8056a5304a2998ce8c490
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2021-07-15 22:26:08 +0200

    Removed onLoad and checkCompatibility

--- js/pixel-editor.js
+++ js/pixel-editor.js
@@ -65,3 +65,54 @@
 
 // Controls execution of this preset module
 PresetModule.instrumentPresetMenu();
+
+//when the page is done loading, you can get ready to start
+window.onload = function () {
+    featureToggles.onLoad();
+
+    currentTool.updateCursor();
+	
+	//check if there are any url parameters
+	if (window.location.pathname.replace('/pixel-editor/','').length <= 1)  {
+		//show splash screen
+		Dialogue.showDialogue('splash', false);
+	}
+	//url parameters were specified
+	else {
+		let args = window.location.pathname.split('/');
+		let paletteSlug = args[2];
+		let dimentions = args[3];
+
+		//fetch palette via lospec palette API
+		fetch('https://lospec.com/palette-list/'+paletteSlug+'.json')
+			.then(response => response.json())
+			.then(data => {
+				//palette loaded successfully
+				palettes[paletteSlug] = data;
+				palettes[paletteSlug].specified = true;
+
+				//refresh list of palettes
+				document.getElementById('palette-menu-splash').refresh();
+
+				//if the dimentions were specified
+				if (dimentions && dimentions.length >= 3 && dimentions.includes('x')) {
+					let width = dimentions.split('x')[0];
+					let height = dimentions.split('x')[1];
+					
+					//create new document
+					Startup.newPixel(width, height);
+				}
+				//dimentions were not specified -- show splash screen with palette preselected
+				else {
+					//show splash
+					Dialogue.showDialogue('new-pixel', false);
+				}
+			})
+			//error fetching url (either palette doesn't exist, or lospec is down)
+			.catch((error) => {
+				console.warn('failed to load palette "'+paletteSlug+'"', error);
+				//proceed to splash screen
+				Dialogue.showDialogue('splash', false);
+			});
+	} 
+};
