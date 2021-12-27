a52fc647e33e338e00ef2ecd1db8c9ee87216352
Nicola <47360416+unsettledgames@users.noreply.github.com>
2021-12-27 19:11:59 +0100

    Reduced tool size
    Moved tool size buttons on the right, started lasso tool

---
+++ js/tools/LassoSelectionTool.js
@@ -0,0 +1,29 @@
+class LassoSelectionTool extends SelectionTool {
+    constructor (name, options, switchFunc, moveTool) {
+
+    }
+
+    onStart(mousePos) {
+        super.onStart(mousePos);
+    }
+
+    onDrag(mousePos) {
+        super.onDrag(mousePos);
+    }
+
+    onEnd(mousePos) {
+        super.onEnd(mousePos);
+    }
+
+    onSelect(mousePos) {
+        super.onSelect(mousePos);
+    }
+
+    onDeselect(mousePos) {
+        super.onDeselect(mousePos);
+    }
+
+    drawSelection(mousePos) {
+
+    }
+}
